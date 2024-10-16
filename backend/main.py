c947e493d770687d04300a86607f94b63688e9c2
BenyaminDamircheli <damirchelibenyamin@gmail.com>
2024-10-16 10:09:53 -0700

    refactored some code

--- backend/main.py
+++ backend/main.py
@@ -6,15 +6,14 @@ from geopy.distance import distance
 from fastapi import Body
 import json
 from itertools import groupby
-
+from utils.types import GroupedComplaint, Source
 from utils.data_scraping.data_processor import Processor
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
-
+from utils.complaint_summary.complaint_summary import generate_complaint_summary
 load_dotenv()
-API_KEY = "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"
-client = OpenAI(api_key=API_KEY)
+
 
 app = FastAPI()
 
@@ -29,25 +28,8 @@ app.add_middleware(
 processor = Processor()
 SessionLocal = processor.SessionLocal
 
-class Source(BaseModel):
-    title:str
-    url:str
-    body:str
 
-class GroupedComplaint(BaseModel):
-    id:str
-    coordinates:List[float]
-    sources:List[Source]
 
-class Urgency(BaseModel):
-    score:int
-    explanation:str
-
-class Summary(BaseModel):
-    title:str
-    summary:str
-    urgency:Urgency
-    solutions:str
 
 def group_complaints(complaints):
     def group_key(complaint):
@@ -83,40 +65,6 @@ def group_complaints(complaints):
         for complaint in merge_complaints(list(group))
         ]
 
-
-def generate_complaint_summary(complaint: GroupedComplaint):
-    sources = complaint.sources
-    result = ""
-    for source in sources:
-        result += f"Title: {source.title}\nBody: {source.body[:1000]}\n\n ----------------------\n\n"
-
-    prompt = f"""
-    Analyze the following complaints:
-    {result}
-
-    1. Provide a short title that best describes the complaints.
-    2. Provide a short summary of the complaints (100 words max).
-    3. Assign an urgency score from 1-5 and explain why (50 words max).
-    4. Propose a potential solution(s) to address the complaints, keep this in paragraph form, no lists (100 words max).
-    """
-
-    try:
-        response = client.beta.chat.completions.parse(
-            model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "You are an AI assistant analyzing citizen complaints and proposing solutions THAT ONLY RESPONDS IN JSON FORMAT."},
-            {"role": "user", "content": prompt}
-        ],
-        response_format=Summary
-        )
-    except Exception as e:
-        print("Error generating complaint summary: ", e)
-        return {"error": "Failed to generate complaint summary"}
-
-    content = response.choices[0].message.content
-    loaded = json.loads(content)
-    print(loaded)
-    return loaded
 
 
 @app.get("/")
---
+++ backend/utils/types.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+class Urgency(BaseModel):
+    score:int
+    explanation:str
+
+class Summary(BaseModel):
+    title:str
+    summary:str
+    urgency:Urgency
+    solutions:str
+
+class Source(BaseModel):
+    title:str
+    url:str
+    body:str
+
+class GroupedComplaint(BaseModel):
+    id:str
+    coordinates:List[float]
+    sources:List[Source]
