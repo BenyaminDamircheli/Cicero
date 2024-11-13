b845f98c1b6ec8b2551462675dc9bb723b3a598d
BenyaminDamircheli <damirchelibenyamin@gmail.com>
2024-11-13 00:15:03 -0800

    final

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -8,13 +8,13 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-db_user = os.environ.get("user", "postgres.zeaafwuqllrqcvytkhaf")
-db_password = os.environ.get("password", "Benyamin2024!")
-db_host = os.environ.get("host", "aws-0-us-west-1.pooler.supabase.com")
-db_port = os.environ.get("port", "6543")
+db_user = os.environ.get("user")
+db_password = os.environ.get("password")
+db_host = os.environ.get("host")
+db_port = os.environ.get("port")
 db_name = os.environ.get("dbname", "postgres")
 
-DATABASE_URL = "postgresql://postgres.zeaafwuqllrqcvytkhaf:Benyamin2024!@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -38,7 +38,7 @@ class Complaint(Base):
 
     summary = relationship("ComplaintSummary", back_populates="complaints")
 
-# this is the summary of multiple complaints
+# this is the summary of multiple complaints in a specific location about a specific issue
 class ComplaintSummary(Base):
     __tablename__ = "complaint_summaries"
     id = Column(Integer, primary_key=True, index=True)
